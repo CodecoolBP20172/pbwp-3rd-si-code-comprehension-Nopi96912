5bcb6953ceba1fb6ee85264039eece52d5324187
nopi96912 <nopiwork@gmail.com>
2017-06-26 14:23:17 +0200

    Commented the lines

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,37 @@
-import random
+# This program is a Guess a Number game
+import random  # Import module 'random'
 
-guessesTaken = 0
+guessesTaken = 0  # Assign 0 to guessesTaken variable
 
-print('Hello! What is your name?')
-myName = input()
+print('Hello! What is your name?')  # Print a string
+myName = input()  # Assign the input to myName variable
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+number = random.randint(1, 20)  # Assign a number between 1 and 20 to number variable
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # Print string with the myName
+# variable inside it
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+while guessesTaken < 6:  # Execute the code under it while guessesTaken variable don't reach 6
+    print('Take a guess.')  # Print a string
+    guess = input()  # Assign the input to guess variable
+    guess = int(guess)  # Change the guess variable to an integer
 
-    guessesTaken += 1
+    guessesTaken += 1  # Add 1 to the guessesTaken variable
 
-    if guess < number:
-        print('Your guess is too low.')
+    if guess < number:  # See that guess variable is lower than number variable
+        print('Your guess is too low.')  # Print a string
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess > number:  # See that guess variable is higher than number variable
+        print('Your guess is too high.')  # Print a string
 
-    if guess == number:
-        break
+    if guess == number:  # See that guess variable is equal to number variable
+        break  # Break the while loop
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+if guess == number:  # See that guess variable is equal to number variable
+    guessesTaken = str(guessesTaken)  # Change the guess variable to a string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # Print string with
+# myName and guessesTaken variables in it
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess != number:  # See that guess variable is not equal to number variable
+    number = str(number)  # Change the number variable to a string
+    print('Nope. The number I was thinking of was ' + number)  # Print string with number variable
+# at the end of the string
